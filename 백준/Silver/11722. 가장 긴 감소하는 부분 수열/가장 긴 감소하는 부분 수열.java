7664e14ca630fe973af069f6f194b8e9097cd4ca
cryingdryice <104723064+cryingdryice@users.noreply.github.com>
2024-01-15 11:20:41 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 164 ms, Memory: 16832 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String [] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        int nums[] = new int[1001];
+        int dp[] = new int[1001];
+        String[] input = br.readLine().split(" ");
+        
+        for(int i = 0; i<T; i++){
+            nums[i] = Integer.parseInt((input[i]));
+        }
+
+        for(int i = 0; i<T; i++){
+            dp[i] = 1;
+            for(int j = 0; j<i; j++){
+                if(nums[j] > nums[i] && dp[j]+1 > dp[i]){
+                    dp[i] = dp[j]+1;
+                }
+            }
+        }
+        int max = 0;
+        for(int i = 0; i<T; i++){
+            max = dp[i] > max ? dp[i] : max;
+        }
+
+        bw.write(max+"\n");
+        bw.flush();
+        bw.close();
+    }
+}
