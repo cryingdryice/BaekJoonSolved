92a216ab85355197f7bd927c3653583db168b0e0
cryingdryice <104723064+cryingdryice@users.noreply.github.com>
2024-03-17 14:34:53 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.87 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -3,34 +3,28 @@ class Solution {
     public int[] solution(int[] numbers) {
         HashSet<Integer> numsSet = new HashSet<Integer>();
 
-//      모든 조합 구하기
         for(int i = 0; i<numbers.length - 1; i++){
             for(int j = i+1; j<numbers.length; j++){
                 numsSet.add(numbers[i]+numbers[j]);
             }
         }
 
-//      answer배열에 조합 복사
         int[] answer = new int[numsSet.size()];
         int k = 0;
         for (Integer i : numsSet) {
             answer[k++] = i;
         }
 
-//      삽입정렬(데이터가 적은경우에 효과적)
-        insertionSort(answer);
-
-        return answer;
-    }
-    public void insertionSort(int[] arr) {
-        int N = arr.length;
+        int N = answer.length;
         int temp;
         for (int i = 1; i < N; i++) {
-            for (int j = i; j > 0 && arr[j]< arr[j-1]; j--) {
-                temp = arr[j];
-                arr[j] = arr[j-1];
-                arr[j-1] = temp;
+            for (int j = i; j > 0 && answer[j]< answer[j-1]; j--) {
+                temp = answer[j];
+                answer[j] = answer[j-1];
+                answer[j-1] = temp;
             }
         }
+        
+        return answer;
     }
 }
