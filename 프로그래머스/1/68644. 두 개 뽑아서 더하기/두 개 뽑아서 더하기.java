ae4ea10b095096c1534f28b797237b4316903ec2
cryingdryice <104723064+cryingdryice@users.noreply.github.com>
2024-03-17 14:30:04 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.17 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -0,0 +1,42 @@
+import java.util.HashSet;
+class Solution {
+    public int[] solution(int[] numbers) {
+        HashSet<Integer> numsSet = new HashSet<Integer>();
+
+//      모든 조합 구하기
+        for(int i = 0; i<numbers.length - 1; i++){
+            for(int j = i+1; j<numbers.length; j++){
+                numsSet.add(numbers[i]+numbers[j]);
+            }
+        }
+
+//      answer배열에 조합 복사
+        int[] answer = new int[numsSet.size()];
+        int k = 0;
+        for (Integer i : numsSet) {
+            answer[k++] = i;
+        }
+
+//      삽입정렬(데이터가 적은경우에 효과적)
+        insertionSort(answer);
+
+
+//      배열 출력
+        for (int i : answer){
+            System.out.println(i);
+        }
+
+        return answer;
+    }
+    public void insertionSort(int[] arr) {
+        int N = arr.length;
+        int temp;
+        for (int i = 1; i < N; i++) {
+            for (int j = i; j > 0 && arr[j]< arr[j-1]; j--) {
+                temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+            }
+        }
+    }
+}
