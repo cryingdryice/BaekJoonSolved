cde42cc9235469be63b3f9cb0b399097543f60d1
Jiwon Park <104723064+cryingdryice@users.noreply.github.com>
2025-01-29 18:26:47 +0900

    [Gold V] Title: 치킨 배달, Time: 276 ms, Memory: 24424 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,90 +1,78 @@
-// 코딩테스트 템플릿
-
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String args[])throws IOException {
+    static ArrayList<ArrayList<Integer>> homes = new ArrayList<>();
+    static ArrayList<ArrayList<Integer>> chickens = new ArrayList<>();
+    static ArrayList<Integer> sums = new ArrayList<>();
+
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String[] input = br.readLine().split(" ");
         int N = Integer.parseInt(input[0]);
         int M = Integer.parseInt(input[1]);
-
-        ArrayList<ArrayList<Integer>> homes = new ArrayList<>();
-        ArrayList<ArrayList<Integer>> chickens = new ArrayList<>();
         int temp;
 
-        for(int i = 0; i<N; i++){
+        for (int i = 0; i < N; i++) {
             input = br.readLine().split(" ");
-            for(int j = 0; j<N; j++){
+            for (int j = 0; j < N; j++) {
                 temp = Integer.parseInt(input[j]);
-                ArrayList<Integer> al = new ArrayList<Integer>(Arrays.asList(i, j));
-                if(temp == 1){
+                ArrayList<Integer> al = new ArrayList<>(Arrays.asList(i, j));
+                if (temp == 1) {
                     homes.add(al);
                 }
-                if(temp == 2){
+                if (temp == 2) {
                     chickens.add(al);
                 }
             }
         }
 
         int min = Integer.MAX_VALUE;
-        boolean[] visisted = new boolean[chickens.size()];
-        ArrayList<Integer> sums = new ArrayList<Integer>();
-        bt(chickens, homes,0, visisted, M, sums);
-        for(int sum : sums){
+        boolean[] visited = new boolean[chickens.size()];
+        bt(0, visited, M);
+        
+        for (int sum : sums) {
             min = Math.min(min, sum);
         }
 
-
-        bw.write(min+"\n");
-
+        bw.write(min + "\n");
         bw.flush();
         bw.close();
     }
-    static void bt(ArrayList<ArrayList<Integer>> chickens, ArrayList<ArrayList<Integer>> homes, int start, boolean[] visited, int M, ArrayList<Integer> sums){
-        if(M == 0){
-            // 치킨집 조합
+
+    static void bt(int start, boolean[] visited, int M) {
+        if (M == 0) {
             ArrayList<ArrayList<Integer>> choices = new ArrayList<>();
-            for(int i = 0; i<chickens.size(); i++){
-                if(visited[i]){
+            for (int i = 0; i < chickens.size(); i++) {
+                if (visited[i]) {
                     choices.add(chickens.get(i));
                 }
             }
 
-            int sum = 0; // 선택된 치킨집에 대한 도시의 치킨 거리
-            for(int i = 0; i<homes.size(); i++){ // 집 순회하며 가장 작은 치킨집 고르기
-                // i번째 집의 최소 치킨 거리
-                int min = distance(choices.get(0), homes.get(i));
-                for(int j = 1; j<choices.size(); j++){
-                    min = Math.min(min, distance(choices.get(j), homes.get(i)));
+            int sum = 0;
+            for (ArrayList<Integer> home : homes) {
+                int min = distance(choices.get(0), home);
+                for (int j = 1; j < choices.size(); j++) {
+                    min = Math.min(min, distance(choices.get(j), home));
                 }
                 sum += min;
             }
 
             sums.add(sum);
-//            System.out.println(sum);
             return;
         }
 
-        for(int i = start; i<chickens.size(); i++){
+        for (int i = start; i < chickens.size(); i++) {
             visited[i] = true;
-            bt(chickens, homes,i+1, visited, M-1, sums);
+            bt(i + 1, visited, M - 1);
             visited[i] = false;
         }
-
-        return;
     }
 
-    static int distance(ArrayList<Integer> choice,ArrayList<Integer> home){
-        int r1 = home.get(0);
-        int c1 = home.get(1);
-        int r2 = choice.get(0);
-        int c2 = choice.get(1);
-
-        return Math.abs(r1-r2)+Math.abs(c1-c2);
+    static int distance(ArrayList<Integer> choice, ArrayList<Integer> home) {
+        return Math.abs(home.get(0) - choice.get(0)) + Math.abs(home.get(1) - choice.get(1));
     }
 }
